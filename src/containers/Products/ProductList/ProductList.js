c840c78a4144e0902aab5c588e8deb0b998252c7
Kulchoro <jumalievkulchoro@gmail.com>
2019-05-12 18:23:17 +0600

    Working with AXIOS

---
+++ src/containers/Products/ProductList/ProductList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./ProductList.module.css";
+
+function ProductList(props) {
+  return (
+    <div className={classes.ProductList}>
+      
+    </div>
+  );
+}
+
+export default ProductList;
--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -2,82 +2,31 @@ import React, { Component } from "react";
 import classes from "./Products.module.css";
 import axios from "../../axios";
 import { Route } from "react-router-dom";
+import ProductList from "./ProductList/ProductList";
 class Products extends Component {
   state = {
-    price: 0,
-    cart: []
+    products: [],
+    cart: 0
   };
+
   componentDidMount() {
-    axios.get("information.json").then(response => {
+    axios.get("products.json").then(response => {
       this.setState({
-        price: response.data
+        products: response.data
       });
     });
   }
-  render() {
-    const price = this.state.price;
-    let res = Object.keys(this.state.price).map(function(key) {
-      return [price[key]];
-    });
 
+  render() {
+    const products = this.state.products;
+    let result = Object.keys(products).map(function(key) {
+      return products[key]
+    });
     return (
       <div className={classes.Products}>
         <div>Cart:{this.state.cart}</div>
-        <div>
-          <h5>Product #1</h5>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
-          animi ipsum facilis recusandae esse ab sunt consequatur incidunt,
-          voluptate velit praesentium quod repudiandae minima modi vel nemo
-          corrupti hic aspernatur. Price: {res}
-          <button>Checkout</button>
-          <button>Add cart</button>
-        </div>
-        <div>
-          <h5>Product #2</h5>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
-          animi ipsum facilis recusandae esse ab sunt consequatur incidunt,
-          voluptate velit praesentium quod repudiandae minima modi vel nemo
-          corrupti hic aspernatur. Price: {res}
-          <button>Checkout</button>
-          <button>Add cart</button>
-        </div>
-        <div>
-          <h5>Product #3</h5>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
-          animi ipsum facilis recusandae esse ab sunt consequatur incidunt,
-          voluptate velit praesentium quod repudiandae minima modi vel nemo
-          corrupti hic aspernatur. Price: {res}
-          <button>Checkout</button>
-          <button>Add cart</button>
-        </div>
-        <div>
-          <h5>Product #4</h5>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
-          animi ipsum facilis recusandae esse ab sunt consequatur incidunt,
-          voluptate velit praesentium quod repudiandae minima modi vel nemo
-          corrupti hic aspernatur. Price: {res}
-          <button>Checkout</button>
-          <button>Add cart</button>
-        </div>
-        <div>
-          <h5>Product #5</h5>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
-          animi ipsum facilis recusandae esse ab sunt consequatur incidunt,
-          voluptate velit praesentium quod repudiandae minima modi vel nemo
-          corrupti hic aspernatur. Price: {res}
-          <button>Checkout</button>
-          <button>Add cart</button>
-        </div>
-        <div>
-          <h5>Product #6</h5>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
-          animi ipsum facilis recusandae esse ab sunt consequatur incidunt,
-          voluptate velit praesentium quod repudiandae minima modi vel nemo
-          corrupti hic aspernatur. Price: {res}
-          <button>Checkout</button>
-          <button>Add cart</button>
-        </div>
-        <Route path="/products" render={() => <strong>Hello!</strong>} />
+        <ProductList product={this.state.products} />
+        <Route path="/products" />
       </div>
     );
   }
